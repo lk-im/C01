5fd53ed790dece4ed2cf982685c2445072b5cbae
lk-im <lkane815@gmail.com>
2025-05-24 15:45:27 +0100

    modifie

--- count_files.sh
+++ count_files.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # Demander à l'utilisateur d'entrer le nom du répertoire
-#echo -n "Entrez le nom du répertoire : "
-read repertoire
+ read repertoire
 
-# Vérifier si le répertoire existe
-#if [ -d "$repertoire" ]; then
-    # Compter le nombre de fichiers dans le répertoire
+# Compter le nombre de fichiers dans le répertoire
     nb_fichiers=$(find "$repertoire" -maxdepth 1 -type f | wc -l)
     echo "Le répertoire '$repertoire' contient $nb_fichiers fichier(s)."
-#fi
