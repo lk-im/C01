04f3a382086b4541b4017317ad3adeedd8280218
lk-im <lkane815@gmail.com>
2025-05-28 14:19:18 +0100

    essai

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -5,16 +5,9 @@
 echo "$@"
 
 # Méthode avec $@ (recommandée pour sa simplicité)
+
 count=1
 for arg in "$@"; do
   echo "Argument $count: $arg"
   ((count++))
 done
-
-# Alternative avec $1 et shift (pour comprendre le fonctionnement)
-# count=1
-# while [ -n "$1" ]; do
-#   echo "Argument $count: $1"
-#   ((count++))
-#   shift
-# done
