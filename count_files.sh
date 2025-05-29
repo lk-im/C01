bb334550e8b78d396a0adc5e4b5031f4747156d5
lk-im <lkane815@gmail.com>
2025-05-29 12:40:55 +0100

    ajout

--- count_files.sh
+++ count_files.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # ce script permet de compter le nombre de fichiers dans un répetoire.
-
 read dossier
 
 # Vérifier si le répertoire existe
-if [ -d "$dossier" ]; then
-# Compter le nombre de fichiers dans le répertoire
-    nb_fichiers=$(ls -1 "$dossier" | wc -l)
+if  nb_fichiers=$(ls -1 "$dossier" | wc -l) ; then
     echo "Le dossier $dossier contient $nb_fichiers fichiers(s)."
-else
-    echo "Le dossier $dossier n'existe pas."
 fi
