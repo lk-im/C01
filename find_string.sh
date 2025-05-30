2420d11dad60dc3ae977ac77b0e07738d5c188e5
lk-im <lkane815@gmail.com>
2025-05-30 23:06:19 +0100

    ajout

--- find_string.sh
+++ find_string.sh
@@ -11,7 +11,7 @@ grep "$chaine" "$fichier"
 
 # Vérifier si la chaîne a été trouvée
 if [ $? -eq 0 ]; then
-  echo "La chaîne '$chaine' a été trouvée dans le fichier $fichier."
+  echo "La chaîne '$chaine' a été trouvée dans $fichier."
 else
-  echo "La chaîne '$chaine' n'a pas été trouvée dans le fichier $fichier."
+  echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
 fi
--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#ce script permet d'afficher l'utilisation du disque de façon lisible.
 df -h
