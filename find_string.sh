fe1c4ca636ef774bc8bdb12b0e72d461cbead508
lk-im <lkane815@gmail.com>
2025-05-31 00:02:20 +0100

    ajout

--- find_string.sh
+++ find_string.sh
@@ -9,9 +9,5 @@ read chaine
 # Utiliser grep pour rechercher la chaîne de caractères dans le fichier
 grep "$chaine" "$fichier"
 
-# Vérifier si la chaîne a été trouvée
-if [ $? -eq 0 ]; then
-  echo "La chaîne '$chaine' a été trouvée dans $fichier."
-else
+
   echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
-fi
