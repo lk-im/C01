6ed435701326984420c3de0a8dc9c05e7b30c1a6
lk-im <lkane815@gmail.com>
2025-05-31 04:32:50 +0100

    ajout

--- find_string.sh
+++ find_string.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Demander à l'utilisateur de saisir le nom du fichier
-read fichier
-
-# Demander à l'utilisateur de saisir la chaîne de caractères à rechercher
+read fichier 
 read chaine
 
 # Utiliser grep pour rechercher la chaîne de caractères dans le fichier
 grep "$chaine" "$fichier"
 
-
   echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
+exit   
