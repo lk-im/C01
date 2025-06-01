c28c9c7973eb02e6b694f736ec267d43a80336ec
lk-im <lkane815@gmail.com>
2025-06-01 04:24:16 +0100

    modi

--- count_files.sh
+++ count_files.sh
@@ -2,10 +2,12 @@
 
 # ce script permet de compter le nombre de fichiers dans un répetoire.
 read myfolder
-count=0
-for  file in $(ls "$myfolder"); do 
-if [ -f "$myfolder/$file" ] ; then 
-count=$(count + 1)
-fi
-done
-echo "Le dossier $myfolder contient $count fichier(s) ."
+#count=0
+#for  file in $(ls "$myfolder"); do 
+#if [ -f "$myfolder/$file" ] ; then 
+#count=$(count + 1)
+#fi
+#done
+count=$(find "$1" -maxdepth 1 -type f | wc -l)
+
+echo "Le dossier $myfolder contient $count fichier(s)."
