912b56aed9eb220678291aaf56839edbca73d8a9
lk-im <lkane815@gmail.com>
2025-06-01 04:30:03 +0100

    modi

--- find_string.sh
+++ find_string.sh
@@ -7,5 +7,5 @@ read chaine
 # Utiliser grep pour rechercher la chaîne de caractères dans le fichier
 #if ! ; then
 grep "$chaine" "$fichier"
-  echo "La chaîne '$chaine' n'a pas été trouvée dans '$fichier'."
+  echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
 #fi
